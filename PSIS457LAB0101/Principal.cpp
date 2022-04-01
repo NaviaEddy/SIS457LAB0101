e6e90910f2ba706314c4911af4c6c06d9d6e66cf
Eddy <Eddy@DESKTOP-TAMIQ82>
2022-04-01 18:27:48 -0400

    Agregar archivos de proyecto.

---
+++ PSIS457LAB0101/Principal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "                               BIOGRAFIA                         " << endl;
+	cout << "Mi nombre es Eddy Navía Condori." << endl;
+	cout << "Tengo 19 años, naci el 25 de mayo del 2003." << endl;
+	cout << "Soy de la Carrera Ing. de Sistemas." << endl;
+	cout << "Egrese del Colegio Nacional Junín." << endl;
+	cout << "Se tocar instrumentos musicales como la guitarra y el teclado." << endl;
+	cout << "Tambien se cantar." << endl;
+	cout << "Dirigo un ministerio de música en la Capilla de la Virgen de la Medalla Milagrosa, de igual manera tambien soy catequista." << endl;
+	cout << "Tengo conocientos basicos sobre lo que es la soldadura de metales." << endl;
+	cout << "Doy clases de matematica escolar." << endl;
+ return 0;
+}
