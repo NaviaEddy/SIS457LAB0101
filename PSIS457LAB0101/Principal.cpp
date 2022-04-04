91093f0a5877fbac0f2ffde643f8d03bc782aaa0
Eddy Navia <eddy.navia.123@hotmail.com>
2022-04-03 21:42:35 -0400

    Cambios2.cpp

--- PSIS457LAB0101/Principal.cpp
+++ PSIS457LAB0101/Principal.cpp
@@ -6,19 +6,21 @@ int main()
 	op = 1;
 	while( op == 1)
 	{
-		cout << "                               BIOGRAFIA                         " << endl;
+		cout << "                     BIOGRAFIA                         " << endl;
 		cout << "Mi nombre es Eddy Navía Condori." << endl;
 		cout << "Tengo 19 años, naci el 25 de mayo del 2003." << endl;
 		cout << "Soy de la Carrera Ing. de Sistemas." << endl;
 		cout << "Egrese del Colegio Nacional Junín." << endl;
-		cout << "Se tocar instrumentos musicales como la guitarra y el teclado." << endl;
-		cout << "Tambien se cantar." << endl;
-		cout << "Dirigo un ministerio de música en la Capilla de la Virgen de la Medalla Milagrosa, de igual manera tambien soy catequista." << endl;
-		cout << "Tengo conocientos basicos sobre lo que es la soldadura de metales." << endl;
-		cout << "Doy clases de matematica escolar." << endl << endl;
 		cout << "¿Desea volver a correr el programa? " << endl;
 		cout << "Presione 1. SI" << endl << "Presione 2. NO" << endl << endl;
 		cin >> op;
+		while (op <= 0 || op >= 3)
+		{
+			cout << "INGRESAR NUMEROS COMPRENDIDOS ENTRE 1 Y 2" << endl << endl << endl;
+			cout << "¿Desea volver a correr el programa? " << endl;
+			cout << "Presione 1. SI" << endl << "Presione 2. NO" << endl << endl;
+			cin >> op;  
+		}
 	}
 	system("cls");
 	return 0;
